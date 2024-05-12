e48bc29c1a2bae85fa2a83ff05e6fac6bf2d88b2
minxd95 <newobj95@gmail.com>
2024-05-13 00:26:21 +0900

    12장 ing

---
+++ vid_player/lib/components/custom_video_player.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
+
+class CustomVideoPlayer extends StatefulWidget {
+  final XFile video;
+
+  const CustomVideoPlayer({
+    required this.video,
+    super.key,
+  });
+
+  @override
+  State<CustomVideoPlayer> createState() => _CustomVideoPlayerState();
+}
+
+class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  VideoPlayerController? videoController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    initializeController();
+  }
+
+  void initializeController() async {
+    final videoController = VideoPlayerController.file(
+      File(widget.video.path),
+    );
+
+    await videoController.initialize();
+
+    setState(() {
+      this.videoController = videoController;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (videoController == null) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    return AspectRatio(
+      aspectRatio: videoController!.value.aspectRatio,
+      child: VideoPlayer(
+        videoController!,
+      ),
+    );
+  }
+}
